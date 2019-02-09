e11c80be17093d459404e9b62236f37df94c875a
Adam Warner <adamw@rner.email>
2019-02-09 02:55:34 +0000

    if ls.exe/curl.exe are available, add remove the powershell aliases
    Signed-off-by: Adam Warner <adamw@rner.email>

--- powershell/Microsoft.Powershell_functions.ps1
+++ powershell/Microsoft.Powershell_functions.ps1
@@ -36,3 +36,33 @@ function ggl { git log --graph --color --all --decorate --format="%C(auto)%d %s"
 function ggll { git log --graph --color --all --decorate --format="%C(auto)%h %d %s %Cblue %ar %an" }
 function ggx { git show -s --format='%Cgreen%h %Cblue%an %Cred%cr%Creset%n%s' }
 function ggroot { pushd (git rev-parse --show-toplevel) }
+
+# Directory Listing: Use `ls.exe` if available
+if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:ls -ErrorAction SilentlyContinue
+    # Set `ls` to call `ls.exe` and always use --color
+    ${function:ls} = { ls.exe --color @args }
+    # List all files in long format
+    ${function:l} = { ls -lF @args }
+    # List all files in long format, including hidden files
+    ${function:la} = { ls -laF @args }
+    # List only directories
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+} else {
+    # List all files, including hidden files
+    ${function:la} = { ls -Force @args }
+    # List only directories
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+}
+
+# curl: Use `curl.exe` if available
+if (Get-Command curl.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:curl -ErrorAction SilentlyContinue
+    # Set `ls` to call `ls.exe` and always use --color
+    ${function:curl} = { curl.exe @args }
+    # Gzip-enabled `curl`
+    ${function:gurl} = { curl --compressed @args }
+} else {
+    # Gzip-enabled `curl`
+    ${function:gurl} = { curl -TransferEncoding GZip }
+}
