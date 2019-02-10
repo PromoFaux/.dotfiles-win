20c938c014fd75388c8fd6741c74219dd0ea0a86
Adam Warner <adamw@rner.email>
2019-02-10 21:04:43 +0000

    iCue is being weird. Will install this manually for now.

--- home/bootstrap.ps1
+++ home/bootstrap.ps1
@@ -2,14 +2,23 @@
 
 . "..\utils.ps1"
 
+# Set Computer Name
+
+$local:newName = "Adam-PC"
+$local:currName = (Get-WmiObject Win32_ComputerSystem).Name
+if (!($local:currName -eq $local:newName)){
+    Write-Output "Changing computername from $local:currName to $local:newName (will be changed after reboot)"
+    (Get-WmiObject Win32_ComputerSystem).Rename($local:newName) | Out-Null
+}
+
 Write-Output ""
 Write-Output "Running Home-specific commands"
 Install steam
 Install geforce-experience
 Install razer-synapse-2
-Install cue
+#Install cue
 Install discord.install
 Install msiafterburner
+Install uplay
 
-lns "$env:APPDATA\corsair\CUE\profiles" ".\iCue\profiles"
 lns "${env:ProgramFiles(x86)}\MSI Afterburner\Profiles" ".\MSIAfterburner\Profiles"
