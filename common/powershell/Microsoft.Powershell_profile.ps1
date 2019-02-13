e284af89d5bbf7d98c7b651a830d3e25675e0c0d
Adam Warner <adamw@rner.email>
2019-02-13 13:38:02 +0000

    some profile changes
    Signed-off-by: Adam Warner <adamw@rner.email>

--- common/powershell/Microsoft.Powershell_profile.ps1
+++ common/powershell/Microsoft.Powershell_profile.ps1
@@ -1,9 +1,3 @@
-Set-PsReadlineOption -EditMode Vi -ViModeIndicator Cursor -HistoryNoDuplicate
-Set-PSReadlineOption -BellStyle None
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineKeyHandler -Key Ctrl+r -Function ReverseSearchHistory -ViMode Insert
-Set-PSReadlineKeyHandler -Key Ctrl+r -Function ReverseSearchHistory -ViMode Command
-
 Push-Location (Split-Path -parent $profile)
 "aliases","extra" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
--- common/powershell/aliases.ps1
+++ common/powershell/aliases.ps1
@@ -24,30 +24,6 @@ function rm-history
 	Clear-Host
 }
 
-# Fuzzy Find History
-
-function fzf-hist {
-	Invoke-Expression (cat (Get-PSReadlineOption | select -ExpandProperty historysavepath) | Invoke-Fzf)
-}
-
-# Git Shortcuts
-
-function ggs { git status }
-function gga($file) { git add $file }
-function ggai { git add --interactive }
-function ggaa { git add --all }
-function ggc($msg) { if($msg) { git commit -m $msg } else { git commit } }
-function ggca { git commit --amend }
-function ggcaa { git commit --amend --no-edit }
-function ggp { git push }
-function ggu { git pull --ff-only }
-function ggd { git diff }
-function ggds { git diff --staged }
-function ggl { git log --graph --color --all --decorate --format="%C(auto)%d %s" }
-function ggll { git log --graph --color --all --decorate --format="%C(auto)%h %d %s %Cblue %ar %an" }
-function ggx { git show -s --format='%Cgreen%h %Cblue%an %Cred%cr%Creset%n%s' }
-function ggroot { pushd (git rev-parse --show-toplevel) }
-
 # Directory Listing: Use `ls.exe` if available
 if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
     rm alias:ls -ErrorAction SilentlyContinue
