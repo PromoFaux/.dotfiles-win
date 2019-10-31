9859c233ff556849f409f7a18720b56729196916
Adam Warner <me@adamwarner.co.uk>
2019-10-31 14:16:08 +0000

    Re-jig
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- work/bootstrap.ps1
+++ work/bootstrap.ps1
@@ -7,25 +7,21 @@ net use F: \\srv2-croydon\Sys 2>&1>null
 net use G: \\srv2-croydon\OTTO 2>&1>null
 
 #Visual Studio
-Install visualstudio2017professional
-Install visualstudio2017-workload-manageddesktop
-Install visualstudio2017-workload-netcoretools
-Install visualstudio2017-workload-netweb
-Install visualstudio2017-workload-data
+Install visualstudio2019professional
 Install vswhere
 Install resharper-ultimate-all
 
-#SSMS
+# #SSMS
 Install sql-server-management-studio
-#Install dbforge-sql-cmpl-std #Not working, manually for now
+# #Install dbforge-sql-cmpl-std #Not working, manually for now
 
-#Tools
+# #Tools
 Install mremoteng
 
 #Config Linking
 $local:confDir = "U:\AdamW\Config"
 lns "$env:APPDATA\mRemoteNG\confCons.xml" "$local:confDir\mRemoteNG\confCons.xml" #config stored on U drive
 # lns "$env:APPDATA\Microsoft\Microsoft SQL Server\140\Tools\Shell\RegSrvr.xml" "$local:confDir\SSMS\RegSrvr.xml"
-lns "C:\ProgramData\Devart\dbForge SQL Complete\Snippets" "$local:confDir\dbForge SQL Complete\Snippets"
+lns "C:\ProgramData\Devart\dbForge SQL Complete\Snippets\Custom" "$local:confDir\dbForgeSnippets"
 
 lns "C:\repos" "U:\AdamW\Repos"
