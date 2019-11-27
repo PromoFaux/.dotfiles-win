3b01adeb6f1851efbe031324932b3e44262795c2
Adam Warner <me@adamwarner.co.uk>
2019-11-27 10:57:57 +0000

    remove application installs from work profie, add symlink for pcanywhere remotes
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- work/bootstrap.ps1
+++ work/bootstrap.ps1
@@ -6,22 +6,11 @@ net use U: \\srv2-croydon\Usersdata 2>&1>null
 net use F: \\srv2-croydon\Sys 2>&1>null
 net use G: \\srv2-croydon\OTTO 2>&1>null
 
-#Visual Studio
-Install visualstudio2019professional
-Install vswhere
-Install resharper-ultimate-all
-
-# #SSMS
-Install sql-server-management-studio
-# #Install dbforge-sql-cmpl-std #Not working, manually for now
-
-# #Tools
-Install mremoteng
-
 #Config Linking
-$local:confDir = "U:\AdamW\Config"
+$local:confDir = "U:\AdamW\config"
 lns "$env:APPDATA\mRemoteNG\confCons.xml" "$local:confDir\mRemoteNG\confCons.xml" #config stored on U drive
 # lns "$env:APPDATA\Microsoft\Microsoft SQL Server\140\Tools\Shell\RegSrvr.xml" "$local:confDir\SSMS\RegSrvr.xml"
 lns "C:\ProgramData\Devart\dbForge SQL Complete\Snippets\Custom" "$local:confDir\dbForgeSnippets"
+lns "C:\ProgramData\Symantec\pcAnywhere\Remotes" "$local:confDir\pcAnywhere\Remotes"
 
 lns "C:\repos" "U:\AdamW\Repos"
