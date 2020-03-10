9f0a4bc8b9ae46c6f2a5764eef3bc36c35a9ebdc
Adam Warner <me@adamwarner.co.uk>
2020-03-10 11:37:16 +0000

    tweaks

--- work/bootstrap.ps1
+++ work/bootstrap.ps1
@@ -11,6 +11,6 @@ $local:confDir = "U:\AdamW\config"
 lns "$env:APPDATA\mRemoteNG\confCons.xml" "$local:confDir\mRemoteNG\confCons.xml" #config stored on U drive
 # lns "$env:APPDATA\Microsoft\Microsoft SQL Server\140\Tools\Shell\RegSrvr.xml" "$local:confDir\SSMS\RegSrvr.xml"
 lns "C:\ProgramData\Devart\dbForge SQL Complete\Snippets\Custom" "$local:confDir\dbForgeSnippets"
-lns "C:\ProgramData\Symantec\pcAnywhere\Remotes" "$local:confDir\pcAnywhere\Remotes"
+#lns "C:\ProgramData\Symantec\pcAnywhere\Remotes" "$local:confDir\pcAnywhere\Remotes"
 
 lns "C:\repos" "U:\AdamW\Repos"
