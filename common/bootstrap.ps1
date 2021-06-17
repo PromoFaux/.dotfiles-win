bf81fdd7558231bb1359d463610b7c2a856ced53
Adam Warner <me@adamwarner.co.uk>
2021-06-17 20:13:25 +0100

    sudo
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- common/bootstrap.ps1
+++ common/bootstrap.ps1
@@ -56,6 +56,8 @@ if ($script:answer -eq 0) {
     scoop install posh-git
     scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json #this is from https://ohmyposh.dev/docs/windows/#installation
 
+    scoop install sudo
+
 
 
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
